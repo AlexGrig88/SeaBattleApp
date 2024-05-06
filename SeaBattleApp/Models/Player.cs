6b4c76a481ae08aa31a67bc0fbde7ee9ee4c6fa5
Aleksandr Grigorenko <grigxia86@gmail.com>
2024-05-06 19:18:39 +0300

    Added class User and Player

--- SeaBattleApp/Models/Player.cs
+++ SeaBattleApp/Models/Player.cs
@@ -6,7 +6,17 @@ using System.Threading.Tasks;
 
 namespace SeaBattleApp.Models
 {
-    internal class Player
+    internal class Player : User<int>
     {
+        public int Score { get; set; }      // за сколько ходов уничтожен корабль
+        public int VictoryCounter { get; set; }
+        public int DefeatCounter { get; set; }
+
+        public Player(int id, string username, string email = "test@yandex.ru", string password = "123") : base(id, username, email, password)
+        {
+            Score = 0;
+            VictoryCounter = 0;
+            DefeatCounter = 0;
+        }
     }
 }
---
+++ SeaBattleApp/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeaBattleApp.Models
+{
+    internal class User<T>
+    {
+        public T Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public User(T id, string username, string email = "test@yandex.ru", string password = "123")
+        {
+            Id = id;
+            Username = username;
+            Email = email;
+            Password = password;
+        }
+    }
+}
