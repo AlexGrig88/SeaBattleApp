5e02987deb51ce3baeefe30867aff1fdc67a2172
Aleksandr Grigorenko <grigxia86@gmail.com>
2024-05-13 22:18:41 +0300

    Добавил фон и меню "Правила игры"

---
+++ SeaBattleGUI/Views/MainWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using System.ComponentModel;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SeaBattleGUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private const string ABOUT = @"Классический морской бой.
+Правила размещения кораблей (флота):
+Игровое поле — обычно квадрат 10×10 у каждого игрока, на котором размещается флот кораблей. Горизонтали обычно нумеруются сверху вниз, а вертикали помечаются буквами слева направо. При этом используются буквы русского алфавита от «А» до «К» (буквы «Ё» и «Й» обычно пропускаются). Размещаются:
+
+1 корабль — ряд из 4 клеток («четырёхпалубный»; линкор)
+2 корабля — ряд из 3 клеток («трёхпалубные»; крейсера)
+3 корабля — ряд из 2 клеток («двухпалубные»; эсминцы)
+4 корабля — 1 клетка («однопалубные»; торпедные катера)\
+
+При размещении корабли не могут касаться друг друга сторонами и углами.
+
+Рядом со «своим» полем чертится «чужое» такого же размера, только пустое. Это участок моря, где плавают корабли противника.
+
+При попадании в корабль противника — на чужом поле ставится крестик, при холостом выстреле — точка. Попавший стреляет ещё раз.
+
+Самыми уязвимыми являются линкор и торпедный катер: первый из-за крупных размеров, в связи с чем его сравнительно легко найти, а второй из-за того, что топится с одного удара, хотя его найти достаточно сложно.";
+        public MainWindow()
+        {
+            InitializeComponent();
+            Closing += MainWindow_Closing;
+        }
+
+        private void MainWindow_Closing(object? sender, CancelEventArgs e)
+        {
+            string msg = "Вы действительно хотите закрыть программу?";
+            var result = MessageBox.Show(msg, Title, MessageBoxButton.YesNo, MessageBoxImage.Warning);
+            if (result == MessageBoxResult.No) {
+                e.Cancel = true;
+            }
+        }
+
+        private void MenuExit_Close(object sender, RoutedEventArgs e) => Close();
+
+        private void ButtonStart_Click(object sender, RoutedEventArgs e)
+        {
+            PlayingField.Visibility = Visibility.Visible;
+            StartingField.Visibility = Visibility.Collapsed;
+        }
+
+        private void AboutMenu_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(ABOUT, Title, MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+    }
+}
